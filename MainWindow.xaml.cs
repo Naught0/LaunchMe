5f50e8a2f4e08bb6733a94d8de40d704b54e62d0
James <>
2020-03-19 00:50:02 -0400

    wow it actually sorta works

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,20 +15,57 @@ using System.Data.SQLite;
 using System.IO;
 using System.Diagnostics;
 using Microsoft.Win32;
-using System.Drawing;
+using System.Runtime.InteropServices;
+using System.Windows.Interop;
 
 namespace LaunchMe
 {
-    public partial class ListItemData : ListViewItem
+    public partial class ListItemData : StackPanel
     {
         public string Display { get; set; }
         public string Data { get; set; }
-        public Icon Icon { get; set; }
-        public ListItemData(string display, string data, Icon icon)
+        public Image Icon { get; set; }
+        public StackPanel MainPane { get; set; }
+        public StackPanel SecondaryPane { get; set; }
+
+        public short Padding { get; set; } = 3;
+
+        public ListItemData(string display, string data, string iconpath, int iconSize) : base()
         {
             Display = display;
             Data = data;
-            Icon = icon;
+            
+            var iconSource = System.Drawing.Icon.ExtractAssociatedIcon(iconpath);
+            Icon = new Image()
+            {
+                Source = Imaging.CreateBitmapSourceFromHIcon(
+                    iconSource.Handle,
+                    Int32Rect.Empty,
+                    BitmapSizeOptions.FromEmptyOptions()),
+                Margin = new Thickness(Padding),
+                Width = iconSize,
+                Height = iconSize,
+            };
+
+            Orientation = Orientation.Vertical;
+
+            MainPane = new StackPanel() { Orientation = Orientation.Horizontal };
+            MainPane.Children.Add(Icon);
+            MainPane.Children.Add(new TextBlock() { Text = Display, VerticalAlignment = VerticalAlignment.Center, Padding = new Thickness(Padding), TextTrimming = TextTrimming.CharacterEllipsis } );
+
+            SecondaryPane = new StackPanel() { Orientation = Orientation.Horizontal };
+            SecondaryPane.Children.Add(new TextBlock()
+            {
+                Text = Data,
+                VerticalAlignment = VerticalAlignment.Center,
+                Padding = new Thickness(Padding),
+                Foreground = (System.Windows.Media.SolidColorBrush)(new System.Windows.Media.BrushConverter().ConvertFrom("#999999")),
+                TextTrimming = TextTrimming.CharacterEllipsis
+            });
+
+            Children.Add(MainPane);
+            Children.Add(SecondaryPane);
+
         }
 
         public override string ToString()
@@ -43,7 +80,7 @@ namespace LaunchMe
     public partial class MainWindow : Window
     {
         string WinPath = Environment.GetEnvironmentVariable("PATH");
-        public double InternalPadding { get; set; }
+        public static double InternalPadding { get; set; }
 
         public int DefaultFontSize { get; set; }
         public System.Windows.Media.FontFamily FontFace { get; set; } = new System.Windows.Media.FontFamily("Segoe UI");
@@ -52,27 +89,79 @@ namespace LaunchMe
 
         List<ListItemData> SearchResults = new List<ListItemData>();
 
-        bool ResultsWindowShown = false;
-
         int MaxResults = 5;
+
+        // Global hotkey shenanigans 
+        [DllImport("user32.dll")]
+        private static extern bool RegisterHotKey(IntPtr hWnd, int id, uint fsModifiers, uint vk);
+
+        [DllImport("user32.dll")]
+        private static extern bool UnregisterHotKey(IntPtr hWnd, int id);
+
+        private const uint MOD_ALT = 0x0001; //ALT
+        private const uint VK_SPACE = 0x20; // SPACE
+        private const int HOTKEY_ID = 90001;
+
+        private HwndSource source;
 
         public MainWindow()
         {
             InitializeComponent();
             var res = GetResolution();
             Width = res[0] * .22;
-            Height = res[1] * .18;
+            Height = res[1] * .2;
             InternalPadding = Height * 0.8;
-
-            InitDB();
+            
+            if (!File.Exists("Apps.sqlite"))
+            {
+                InitDB();
+            }
             ScanPrograms();
 
-            userInput.FontFamily = FontFace;
-            userInput.FontSize = Height * 0.3;
-            userInput.FontWeight = FontWeight.FromOpenTypeWeight(300);
+            userInput.FontFamily = previewResult.FontFamily = FontFace;
+            userInput.FontSize = previewResult.FontSize = Height * 0.25;
+            userInput.FontWeight = previewResult.FontWeight = FontWeight.FromOpenTypeWeight(300);
             userInput.Focus();
 
             listResults.ItemsSource = SearchResults;
+        }
+
+        protected override void OnSourceInitialized(EventArgs e)
+        {
+            // https://social.technet.microsoft.com/wiki/contents/articles/30568.wpf-implementing-global-hot-keys.aspx
+            base.OnSourceInitialized(e);
+
+            IntPtr handle = new WindowInteropHelper(this).Handle;
+            source = HwndSource.FromHwnd(handle);
+            source.AddHook(HwndHook);
+
+            RegisterHotKey(handle, HOTKEY_ID, MOD_ALT, VK_SPACE);
+        }
+
+        private IntPtr HwndHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            // https://social.technet.microsoft.com/wiki/contents/articles/30568.wpf-implementing-global-hot-keys.aspx
+            const int WM_HOTKEY = 0x0312;
+            switch (msg)
+            {
+                case WM_HOTKEY:
+                    switch (wParam.ToInt32())
+                    {
+                        case HOTKEY_ID:
+                            int vkey = (((int)lParam >> 16) & 0xFFFF);
+                            if (vkey == VK_SPACE)
+                            {
+                                if (WindowState == WindowState.Minimized)
+                                {
+                                    WindowState = WindowState.Normal;
+                                }
+                            }
+                            handled = true;
+                            break;
+                    }
+                    break;
+            }
+            return IntPtr.Zero;
         }
 
         private void InitDB()
@@ -85,7 +174,7 @@ namespace LaunchMe
             var sql = "CREATE TABLE 'applications'( "
             + "'id'    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE, "
             + "'name'  TEXT NOT NULL, "
-            + "'path'  TEXT NOT NULL" 
+            + "'path'  TEXT NOT NULL UNIQUE" 
             + ");";
             SQLiteCommand cmd = new SQLiteCommand(sql, connection);
             cmd.ExecuteNonQuery();
@@ -139,8 +228,8 @@ namespace LaunchMe
                     {
                         await cmd.ExecuteNonQueryAsync();
                     }
-                    catch (SQLiteException ex){
-                        MessageBox.Show("\n\n" + cmd.Parameters[0].Value.ToString());
+                    catch (SQLiteException){
+                        return;
                     }
                 }
             }
@@ -157,22 +246,37 @@ namespace LaunchMe
         {
             if (e.Key == Key.Escape)
             {
-                Close();
+                //Close();
+                WindowState = WindowState.Minimized;
             }
             if (e.Key == Key.Enter)
             {
                 try
                 {
-                    Process.Start(SearchResults[listResults.SelectedIndex].Data);
+                    Process.Start(listResults.SelectedIndex == -1 ? SearchResults[0].Data : SearchResults[listResults.SelectedIndex].Data);
                 }
-                catch (IndexOutOfRangeException ex)
+                catch (IndexOutOfRangeException)
                 {
                     listResults.SelectedIndex = 0;
                 }
-                catch
+                catch (Exception ex)
                 {
-                    return;
+                    MessageBox.Show(ex.ToString());
                 }
+                finally
+                {
+                    WindowState = WindowState.Minimized;
+                }
+            }
+            if (e.Key == Key.Down)
+            {
+                if (listResults.SelectedIndex == SearchResults.Count) { return; }
+                listResults.SelectedIndex ++;
+            }
+            if (e.Key == Key.Up)
+            {
+                if (listResults.SelectedIndex == -1) { return; }
+                listResults.SelectedIndex --;
             }
         }
 
@@ -182,6 +286,7 @@ namespace LaunchMe
             if (userInput.Text.Length < 3)
             {
                 listResults.Opacity = 0;
+                previewResult.Text = string.Empty;
                 return;
             }
 
@@ -197,7 +302,7 @@ namespace LaunchMe
                     var reader = await cmd.ExecuteReaderAsync();
                     while (reader.Read())
                     {
-                        SearchResults.Add(new ListItemData(reader.GetString(0), reader.GetString(1), System.Drawing.Icon.ExtractAssociatedIcon(reader.GetString(1))));
+                        SearchResults.Add(new ListItemData(reader.GetString(0), reader.GetString(1), reader.GetString(1), 24));
                     }
                 }
                 connection.Close();
@@ -207,6 +312,14 @@ namespace LaunchMe
                 listResults.SelectedIndex = 0;
                 listResults.Opacity = 1;
                 listResults.ItemsSource = SearchResults;
+                if (listResults.SelectedIndex == -1)
+                {
+                    previewResult.Text = SearchResults[0].Display;
+                }
+                else
+                {
+                    previewResult.Text = SearchResults[listResults.SelectedIndex].Display;
+                }
                 // TODO
                 // searchIcon.Source; change to app icon later
                 //searchIcon.Source = new BitmapImage();
@@ -231,6 +344,7 @@ namespace LaunchMe
             {
                 // clear list
                 //listResults.Items.Clear();
+                previewResult.Text = string.Empty;
                 listResults.Opacity = 0;
             }
         }
@@ -241,6 +355,20 @@ namespace LaunchMe
             {
                 e.Handled = true;
             }
+        }
+
+        private void UserInput_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+        {
+            e.Handled = true;
+        }
+
+        private void ListResults_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            try
+            {
+                previewResult.Text = SearchResults[listResults.SelectedIndex].Display;
+            }
+            catch { }
         }
     }
 }
